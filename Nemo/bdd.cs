2c5d51e2fb29771cd13e689c086f2bae77c74671
rpoulet <rpoulet@ST247-03>
2024-12-09 11:42:59 +0100

    Ajoutez des fichiers projet.

---
+++ Nemo/bdd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nemo
+{
+    internal class bdd
+    {
+        #region Initialize
+
+        #endregion
+
+        #region OpenConnection
+
+        #endregion
+
+        #region CloseConnection
+
+        #endregion
+    }
+}
